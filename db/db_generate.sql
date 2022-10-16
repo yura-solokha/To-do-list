fc7fa45913f0e040c50b60fd9bec4c2fdb7db82d
yura-solokha <92300715+yura-solokha@users.noreply.github.com>
2022-10-16 22:04:46 +0300

    Add files via upload

---
+++ db/db_generate.sql
@@ -0,0 +1,38 @@
+DROP database IF EXISTS todo;
+
+CREATE DATABASE todo
+    WITH 
+    ENCODING = 'UTF8';
+
+USE todo;
+
+CREATE TABLE users
+(
+    id integer NOT NULL,
+    email character varying(126) NOT NULL,
+    password character varying(8) NOT NULL,
+    PRIMARY KEY (id),
+    CONSTRAINT uemail UNIQUE (email)
+);
+
+CREATE TABLE categories
+(
+    id integer NOT NULL,
+    name character varying(56) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE tasks
+(
+    id integer NOT NULL,
+    name character varying(126) NOT NULL,
+    is_done boolean NOT NULL,
+    priority int NOT NULL,
+    category_id integer,
+    PRIMARY KEY (id),
+    CONSTRAINT fc_category FOREIGN KEY (category_id)
+        REFERENCES public.categories (id) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION
+        NOT VALID
+);
