4ab3cbaf4e775bc80fea4f48aaf45449b5794f46
yura-solokha <solohay2003@gmail.com>
2022-11-03 17:09:48 +0200

    added wpf windows, created project architecture

---
+++ ToDoList/ToDoList/Categories_list.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace tasks_window
+{
+    /// <summary>
+    /// Логика взаимодействия для Categories_list.xaml
+    /// </summary>
+    public partial class Categories_list : Window
+    {
+        public Categories_list()
+        {
+            InitializeComponent();
+        }
+
+        private void Cansel_button_Click(object sender, RoutedEventArgs e)
+        {
+            penis.Text = "101";
+        }
+
+        private void Save_button_Click(object sender, RoutedEventArgs e)
+        {
+            penis.Text = "102";
+        }
+    }
+}
